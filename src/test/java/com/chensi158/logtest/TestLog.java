cf48d0ce506c5e6d4e51a909ca18fa4e1e3cd6d3
chensi158 <chensi158@jd.com>
2021-04-23 19:06:30 +0800

    增加异步配置

--- src/test/java/com/chensi158/logtest/TestLog.java
+++ src/test/java/com/chensi158/logtest/TestLog.java
@@ -19,8 +19,8 @@ public class TestLog {
         ExecutorService pool = new ThreadPoolExecutor(64, 64,
                 0L, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<Runnable>());
-        Logger log = LogManager.getLogger(TestController.class.getName());
-        int size = 10;
+        Logger log = LogManager.getLogger("com.chensi158.logtest.controller.TestController");
+        int size = 1000000;
         CountDownLatch countDownLatch = new CountDownLatch(size);
         for(int i = 0; i < size; i++) {
             LogTask task = new LogTask("index-" + i, log, countDownLatch);
@@ -29,6 +29,24 @@ public class TestLog {
         countDownLatch.await();
         long endTime = System.currentTimeMillis();
         System.out.println("1000w条日志通过64线程池同步打印耗时：" + (endTime - startTime) + "ms");
+    }
+
+    @Test
+    public void testASyncLog() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        ExecutorService pool = new ThreadPoolExecutor(64, 64,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        Logger log = LogManager.getLogger("RollingFile2");
+        int size = 1000000;
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+        for(int i = 0; i < size; i++) {
+            LogTask task = new LogTask("index-" + i, log, countDownLatch);
+            pool.submit(task);
+        }
+        countDownLatch.await();
+        long endTime = System.currentTimeMillis();
+        System.out.println("1000w条日志通过64线程池异步打印耗时：" + (endTime - startTime) + "ms");
     }
 
     static class LogTask implements Runnable {
